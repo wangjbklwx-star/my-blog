ecad008ad008afa2e25456cb3eaed0700572e497
wangjbklwx-star <wangjb.klwx@gmail.com>
2026-02-05 22:44:23 +0800

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL ?? 'https://wangjb.appinn.me',
-  generateRobotsTxt: false,
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://wangjb.appinn.me',
+  generateRobotsTxt: true,
 };
