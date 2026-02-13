2a82205f1e6de0c7eba17098ff225a2281d8ff42
wangjbklwx-star <wangjb.klwx@gmail.com>
2026-02-13 20:42:42 +0800

    Update layout.config.tsx

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,6 +1,6 @@
 import type { LinkItemType } from 'fumadocs-ui/layouts/links';
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { Newspaper, Rss, Tags } from 'lucide-react';
+import { MessageSquare, Newspaper, Rss, Tags } from 'lucide-react';
 
 export const title = '王浚博的博客';
 export const description = "有什么好玩————科技划界，拒绝平庸";
@@ -23,6 +23,13 @@ export const linkItems: LinkItemType[] = [
     text: 'Tags',
     url: '/tags',
     active: 'url',
+  },
+  {
+    icon: <MessageSquare />,
+    text: '论坛',
+    url: 'https://bb.wangjb.eu.cc/',
+    active: 'url',
+    external: true,
   },
   {
     type: 'icon',
